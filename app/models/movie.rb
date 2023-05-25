f30282f5fff8f15eb43942a275f6d95463f1d987
florian autricque <fautricque@gmail.com>
2023-05-25 12:19:33 -0400

    models done

---
+++ app/models/movie.rb
@@ -0,0 +1,10 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+  validates :title, uniqueness: true, presence: true
+  validates :overview, presence: true
+end
+
+# has_many :bookmarks, dependent: :restrict_with_error
+# has_many :lists, through: :bookmarks
